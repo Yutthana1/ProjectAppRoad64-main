0c7b16143d411b0165d43819425301ac1011194b
Yutthana1 <61011212060@msu.ac.th>
2021-01-26 22:45:41 +0700

    Update: ดึงข้อมมูลจาก http get

--- lib/RoadHistory.dart
+++ lib/RoadHistory.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:approad_project64/models/RoadHisrtoryModels.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class roadhistory extends StatefulWidget {
   @override
@@ -6,14 +10,49 @@ class roadhistory extends StatefulWidget {
 }
 
 class _roadhistoryState extends State<roadhistory> {
+
+  List<RoadhistoryModel> RoadHistoryList=[];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LoadRoadhistory();
+  }
+
+  LoadRoadhistory() async {
+    final response = await http.get('http://203.154.83.62:1238/del/kuy/small');
+    if (response.statusCode == 200) {
+      //print(response.statusCode);
+      final jsonDecoDE = jsonDecode(response.body);
+      // print(jsonDecoDE);
+      setState(() {
+        jsonDecoDE.forEach((data) {
+          final dataOBJ = RoadhistoryModel.fromJson(data);
+          if (dataOBJ.uId != '') {
+            RoadHistoryList.add(dataOBJ);
+          }
+          //print(data);
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ประวัติแจ้งถนนชำรุด'),
       ),
-      body: Container(
-        color: Colors.greenAccent,
+      body: ListView.builder(
+        itemCount: RoadHistoryList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('${RoadHistoryList[index].name}'),
+            trailing: Text('${RoadHistoryList[index].nameId}'),
+            subtitle: Text('${RoadHistoryList[index].username}'),
+          );
+        },
       ),
     );
   }
---
+++ lib/models/RoadHisrtoryModels.dart
@@ -0,0 +1,36 @@
+class RoadhistoryModel {
+  Null imgProfileBase64Temp;
+  String name;
+  String nameId;
+  String password;
+  int uId;
+  String username;
+
+  RoadhistoryModel(
+      {this.imgProfileBase64Temp,
+        this.name,
+        this.nameId,
+        this.password,
+        this.uId,
+        this.username});
+
+  RoadhistoryModel.fromJson(Map<String, dynamic> json) {
+    imgProfileBase64Temp = json['img_profile_base64_temp'];
+    name = json['name'];
+    nameId = json['name_id'];
+    password = json['password'];
+    uId = json['u_id'];
+    username = json['username'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['img_profile_base64_temp'] = this.imgProfileBase64Temp;
+    data['name'] = this.name;
+    data['name_id'] = this.nameId;
+    data['password'] = this.password;
+    data['u_id'] = this.uId;
+    data['username'] = this.username;
+    return data;
+  }
+}
